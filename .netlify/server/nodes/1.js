311edec0b576db422bf7547bde32c73fee950d97
Michal <michal.maciak22@gmail.com>
2023-06-07 15:15:21 +0200

    0.0.2.1

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.6d95b24d.js';
-export const imports = ["_app/immutable/entry/error.svelte.6d95b24d.js","_app/immutable/chunks/index.2668702a.js","_app/immutable/chunks/singletons.c39c6a08.js"];
-export const stylesheets = [];
-export const fonts = [];
